fe5a6b9a00e0279ee862ce2dd3fda6129bf6ffa4
Carlos Guanuco <guanucocarlos2@gmail.com>
2025-04-30 11:41:57 -0300

    Luciano Giron y Carlos Guanuco

--- src/assets/components/Producto.jsx
+++ src/assets/components/Producto.jsx
@@ -1,4 +1,5 @@
 import mostrar_objetos from "./funcion1_EJ_AD"  //importa la funcion para mostrar los objetos
+import agregarIVA from "./funcion3_EJ_AD";  //punto 3
 
 function Producto(){
 
@@ -28,6 +29,11 @@ function Producto(){
     const productos = [ producto1, producto2, producto3, producto4, producto5];
     
     mostrar_objetos(productos); //ejercicio 1
+
+    const productosFiltrados = productos.filter(p => p.precio > 20);
+
+    agregarIVA(productos);
+
 }
 
 export default Producto;    // exporto el componente producto
