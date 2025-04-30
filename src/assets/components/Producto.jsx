e1a279c760990f62a611bcecac580ba446e4c612
luciano giron <lucianogiron110@gmail.com>
2025-04-30 07:28:49 -0300

    Ejercicio 3 adicional Luciano Giron y Carlos Guanuco

---
+++ src/assets/components/Producto.jsx
@@ -0,0 +1,29 @@
+function Producto(){
+
+    //DECLARACION DE OBJETOS
+    const producto1 = {
+        descripcion: "celular",
+        precio: 250,
+    }
+    const producto2 = {
+        descripcion: "laptop",
+        precio: 600,
+    }
+    const producto3 = {
+        descripcion: "tablet",
+        precio: 180,
+    }
+    const producto4= {
+        descripcion: "mouse",
+        precio: 40,
+    }
+    const producto5 = {
+        descripcion: "monitor",
+        precio: 150,
+    }
+
+    const productos = [ producto1, producto2, producto3, producto4, producto5];
+    return 10;
+}
+
+export default Producto;    // exporto el componente producto
