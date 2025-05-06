e78b146c1c643f6ef6e25aa9ca0ef23c68a72b4d
LisandroPR <perezruartelisandro@gmail.com>
2025-05-05 23:07:15 -0300

    Ejercicio 3 terminado, Lisandro Perez y Pablo Garzon

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
-
+import "./App.css";
 import Tareas from './assets/components/tareas';
-
+import { useState } from 'react'; 
 import Producto from './assets/components/Producto'   //Importo el componente producto
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
-    <div>
-      <h1>Lista de Tareas</h1>
-      <Tareas />
+    <div className="ambos">
+      <div className="tareas">
+        <Tareas />
+      </div>
+      <div className="productos">
+        <Producto />
+        <h1>Productos</h1>
+      </div>
     </div>
   );
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <Producto/>    {/* Se renderiza Producto*/}
-    </>
-  );
-}
+};
 
 export default App;
 
--- src/assets/components/tareas.jsx
+++ src/assets/components/tareas.jsx
@@ -5,20 +5,34 @@ import TaskList from './TaskList';
 const Tareas = () => {
     const [tasks, setTasks] = useState([]);
 
-    const addTask = (taskText) => {
+    const addTask = (text) => {
         const newTask = {
             id: Date.now(),
-            text: taskText,
+            text,
+            completed: false,
         };
         setTasks([...tasks, newTask]);
-        };
+    };
+
+    const cambiarEstado = (id) => {
+        setTasks(
+            tasks.map((task) =>
+                task.id === id ? { ...task, completed: !task.completed } : task
+        )
+        );
+    };
+
+    const borrar = (id) => {
+        setTasks(tasks.filter((task) => task.id !== id));
+    };
 
     return (
-    <div>
-        <TaskInput onAddTask={addTask} />
-        <TaskList tasks={tasks} />
-    </div>
+        <div>
+            <h1>Lista de Tareas</h1>
+            <TaskInput onAdd={addTask} />
+            <TaskList tasks={tasks} cambiarEstado={cambiarEstado} borrar={borrar} />
+        </div>
     );
-};
+    };
 
 export default Tareas;
